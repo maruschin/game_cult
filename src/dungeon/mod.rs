58d91918211a4adb293954164c0763579432ce77
Evgeny Maruschenko <maruschin@gmail.com>
2023-08-09 13:13:41 +1000

    Create movable camera

--- src/dungeon/mod.rs
+++ src/dungeon/mod.rs
@@ -22,7 +22,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 
     // floor
-    for x_idx in -1..2 {
+    for x_idx in -3..4 {
         for z_idx in -1..2 {
             commands.spawn(SceneBundle {
                 scene: asset_server.load("models/tileBrickB_medium.gltf.glb#Scene0"),
@@ -32,10 +32,38 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         }
     }
 
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("models/wall_door.gltf.glb#Scene0"),
+        transform: Transform::from_xyz(-9.0, 0.0, 2.0),
+        ..default()
+    });
     for x_idx in -1..2 {
         commands.spawn(SceneBundle {
             scene: asset_server.load("models/wall_gate.gltf.glb#Scene0"),
-            transform: Transform::from_xyz((x_idx * 4) as f32, 0.0, 6.0),
+            transform: Transform::from_xyz((x_idx * 4) as f32, 0.0, 2.0),
+            ..default()
+        });
+    }
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("models/wall_end.gltf.glb#Scene0"),
+        transform: Transform::from_xyz(6.0, 0.0, 2.0),
+        ..default()
+    });
+
+    for x_idx in -3..4 {
+        commands.spawn(SceneBundle {
+            scene: asset_server.load("models/wall.gltf.glb#Scene0"),
+            transform: Transform::from_xyz((x_idx * 4) as f32, 0.0, -6.0),
+            ..default()
+        });
+
+        commands.spawn(SceneBundle {
+            scene: asset_server.load("models/wall.gltf.glb#Scene0"),
+            transform: Transform {
+                translation: Vec3::new((x_idx * 4) as f32, 0.0, 6.0),
+                rotation: Quat::from_rotation_y(PI),
+                ..default()
+            },
             ..default()
         });
     }
@@ -50,7 +78,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             shadows_enabled: true,
-            illuminance: 20000.,
+            illuminance: 15000.,
             ..default()
         },
         transform: Transform {
@@ -63,8 +91,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 fn gizmos_system(mut gizmos: Gizmos) {
-    for x_idx in -5..6 {
-        for z_idx in -5..6 {
+    for x_idx in -4..5 {
+        for z_idx in -4..5 {
             gizmos.cuboid(
                 Transform::from_xyz((x_idx * 4) as f32, 2.0, (z_idx * 4) as f32)
                     .with_scale(Vec3::splat(4.)),
@@ -74,8 +102,38 @@ fn gizmos_system(mut gizmos: Gizmos) {
     }
 }
 
-fn rotate_camera(mut query: Query<&mut Transform, With<Camera>>, time: Res<Time>) {
-    let mut transform = query.single_mut();
+fn rotate_camera(
+    mut camera_transform_query: Query<&mut Transform, With<Camera>>,
+    keys: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let mut camera_transform = camera_transform_query.single_mut();
+    let time_delta_rotation = time.delta_seconds() / 2.0;
+    let time_delta_move = time_delta_rotation * 16.0;
 
-    transform.rotate_around(Vec3::ZERO, Quat::from_rotation_y(time.delta_seconds() / 2.));
+    if keys.pressed(KeyCode::Q) {
+        camera_transform.rotate_around(Vec3::ZERO, Quat::from_rotation_y(time_delta_rotation));
+    }
+    if keys.pressed(KeyCode::E) {
+        camera_transform.rotate_around(Vec3::ZERO, Quat::from_rotation_y(-time_delta_rotation));
+    }
+
+    if keys.pressed(KeyCode::W) {
+        let mut forward_vector = camera_transform.forward();
+        forward_vector.y = 0.0;
+        camera_transform.translation += forward_vector.normalize() * time_delta_move;
+    }
+    if keys.pressed(KeyCode::S) {
+        let mut back_vector = camera_transform.back();
+        back_vector.y = 0.0;
+        camera_transform.translation += back_vector.normalize() * time_delta_move;
+    }
+    if keys.pressed(KeyCode::A) {
+        camera_transform.translation =
+            camera_transform.translation + camera_transform.left() * time_delta_move;
+    }
+    if keys.pressed(KeyCode::D) {
+        camera_transform.translation =
+            camera_transform.translation + camera_transform.right() * time_delta_move;
+    }
 }
